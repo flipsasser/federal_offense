54a9d2d1361d72bd6ff8cf619c6b05df4cee0f8e
Flip Sasser <flip@inthebackforty.com>
2020-06-19 11:04:09 -0700

    Prevent crash when loading in projects that do not use ActionCable

--- lib/federal_offense/engine.rb
+++ lib/federal_offense/engine.rb
@@ -34,24 +34,26 @@ module FederalOffense
 
     # Enable ActionCable connections in the inbox for auto-reload
     initializer "action_cable_connection" do |app|
-      # Find cable configs
-      federal_offense_config_path = Rails.root.join("config", "federal_offense_cable.yml")
-      app_config_path = Rails.root.join("config", "cable.yml")
+      if defined? ActionCable
+        # Find cable configs
+        federal_offense_config_path = Rails.root.join("config", "federal_offense_cable.yml")
+        app_config_path = Rails.root.join("config", "cable.yml")
 
-      # Determine which config we're going to use
-      config = if File.exist?(federal_offense_config_path)
-        app.config_for(federal_offense_config_path).with_indifferent_access
-      elsif File.exist?(app_config_path)
-        app.config_for(app_config_path).with_indifferent_access
-      else
-        # Default to an async cable cause whatever
-        {adapter: "async"}
+        # Determine which config we're going to use
+        config = if File.exist?(federal_offense_config_path)
+          app.config_for(federal_offense_config_path).with_indifferent_access
+        elsif File.exist?(app_config_path)
+          app.config_for(app_config_path).with_indifferent_access
+        else
+          # Default to an async cable cause whatever
+          {adapter: "async"}
+        end
+
+        # Assign the values to the cable config and create a server
+        self.class.cable_config.cable = config
+        self.class.cable_server = ActionCable::Server::Base.new(config: self.class.cable_config)
+        FederalOffense.action_cable = true
       end
-
-      # Assign the values to the cable config and create a server
-      self.class.cable_config.cable = config
-      self.class.cable_server = ActionCable::Server::Base.new(config: self.class.cable_config)
-      FederalOffense.action_cable = true
     end
   end
 end
